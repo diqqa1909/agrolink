1de2a7404addcf8476830008fc9842a28273445a
sewni <sewnijayawardana@gmail.com>
2025-12-09 09:35:50 +0530

    changing the file directories

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,16 +1,31 @@
 <?php
-    Trait Controller{
-        public function view($name, $data=[]){
+trait Controller
+{
+    public function view($name, $data = [])
+    {
 
-            if(!empty($data))
-                extract($data);
-        
-            $filename = "../app/views/".$name.".view.php";
-            if(file_exists($filename)){
-                require $filename;
-            }else{
-                $filename = "../app/views/404.view.php";
-                require $filename;
+        if (!empty($data))
+            extract($data);
+
+        // Try root level first
+        $filename = "../app/views/" . $name . ".view.php";
+        if (file_exists($filename)) {
+            require $filename;
+            return;
+        }
+
+        // Try role-based subdirectories (admin, buyer, farmer, transporter)
+        $roles = ['admin', 'buyer', 'farmer', 'transporter'];
+        foreach ($roles as $role) {
+            $roleView = "../app/views/{$role}/" . $name . ".view.php";
+            if (file_exists($roleView)) {
+                require $roleView;
+                return;
             }
         }
+
+        // Fallback to 404
+        $filename = "../app/views/404.view.php";
+        require $filename;
     }
+}
